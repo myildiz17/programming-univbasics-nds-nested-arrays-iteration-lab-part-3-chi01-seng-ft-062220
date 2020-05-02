21a9b034280f07eee6819b9fc42bea8c13c6006d
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-05-02 08:37:03 +0000

    Automatically backed up by Learn

--- mine.rb
+++ mine.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   row_index = 0
      while row_index < src.count do
   	  element_index = 0
-  	  while element_index < src[row_index].count do
+  	  while element_index < src[row_index].length do
 
   	    # Array at row_index
   	    # Element of the inner array at element_index
