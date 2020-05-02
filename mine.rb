42e3fa4d8dd19ba6168e42da26d8f9ecde65cbbd
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-05-02 08:35:28 +0000

    Automatically backed up by Learn

--- mine.rb
+++ mine.rb
@@ -1,31 +1,31 @@
-#a=[2,"a",3,"v"]
-#count=0
-#while count < a.size do
-#  if a[count]%1
-#    puts "yes"
-#  end
+def join_nested_strings(src)
+  # src will be an Array of Arrays of Strings and Integers
+  # Combine all Strings present in the AoA into a single value and return it
 
-#  count+=1
-#end
+  outer_results = []
+  row_index = 0
+     while row_index < src.count do
+  	  element_index = 0
+  	  while element_index < src[row_index].count do
 
-#r="selam" % 2
-#puts r
+  	    # Array at row_index
+  	    # Element of the inner array at element_index
+  	    # If the length of the current element is greater than the length of longest_string_element
+  	    # Set longest_string_element to the current element
+        if src[row_index][element_index].class == String
+          outer_results << src[row_index][element_index]
+        end
+  	    element_index += 1
+  	  end
 
+  	  # We have to check every element in each inner array, so after the loop finishes
+  	  # The current value of longest_string_element is pushed into outer_results
+  	  #outer_results << longest_string_element
+  	  row_index += 1
+  	end
 
-
-even_numbers = []
-string = []
-a=[1,2,3,"a",5,"6"]
-count=0
-while count < a.length do
-  if a[count].class == String
-    even_numbers << a[count]
-  end
-
-  count+=1
+  	puts outer_results # => ["Sporty", "Fajita Mix", "Rosemary"]
 end
 
-puts even_numbers
-#puts "10".class
+join_nested_strings
 
-#puts string
