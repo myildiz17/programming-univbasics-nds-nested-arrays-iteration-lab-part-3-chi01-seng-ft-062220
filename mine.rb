56e9ead911ee59585cf52286fce939725cd733ab
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-05-02 07:03:10 +0000

    Automatically backed up by Learn

---
+++ mine.rb
@@ -0,0 +1,16 @@
+#a=[2,"a",3,"v"]
+#count=0
+#while count < a.size do
+#  if a[count]%1
+    puts "yes"
+#  end
+
+#  count+=1
+#end
+
+#r="selam" % 2
+#puts r
+
+
+a = ["2","a"]
+a.all? { |x| x.is_a? Integer }
